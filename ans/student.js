696657b0c6bd44071bcb614aab9ad4cf4fe90149
Vishal Arora <arorav@gmail.com>
2018-03-06 22:28:37 -0500

    changes

---
+++ ans/student.js
@@ -0,0 +1,32 @@
+class Student{                         // Step 2 Declare fake class Notice the capital S
+
+constructor(name,grade){               // Step 2 Constructor
+
+  this.name              = name;       // Step 3 declare public variables(attributes) of the object
+  this.grade             = grade;
+  this.teacher           = undefined;
+  this.email             = undefined;
+
+}                                      // End of constructor
+
+                                       // Step 4 Declare methods of the class
+setTeacher(name){
+  this.teacher           = name;
+}
+
+setEmail(email){
+  this.email             = email;
+}
+
+
+}                                      // end of class Student
+
+
+let ryan = new Student("Ryan","8");    // Step 5 Instantiate object with new keyword
+console.log (ryan.name);               // Prints "Ryan"
+console.log (ryan.grade);              // Prints 8
+ryan.setTeacher("Mrs Simpson");
+console.log (ryan.teacher)             // Prints Mrs Simpson
+
+ryan.teacher = "Mrs Griffin";          // All variables of the fake class  are public can be accessed with . notation
+console.log (ryan.teacher)             // Prints Mrs Griffin
---
+++ lib/readme.md
@@ -0,0 +1,1 @@
+this folder should have all your JavaScript files to be tested
